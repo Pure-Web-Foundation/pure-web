d15dec0c8e677affb2e1c2aa7b415df087e0399f
mvneerven <marc@van-neerven.net>
2024-08-22 09:55:51 +0200

    debounce added

--- src/js/common.js
+++ src/js/common.js
@@ -20,6 +20,29 @@ export function parseHTML(html) {
 }
 
 /**
+ * Debounces events that occur repeatedly, such as resize and mousemove events.
+ * @param {Function} fn
+ */
+export const debounce = (fn) => {
+  // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+  let frame;
+
+  // The debounce function returns a new function that can receive a variable number of arguments
+  return (...params) => {
+    // If the frame variable has been defined, clear it now, and queue for next frame
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+
+    // Queue our function call for the next frame
+    frame = requestAnimationFrame(() => {
+      // Call our function and pass any params we received
+      fn(...params);
+    });
+  };
+};
+
+/**
  * Throttles execution of any function
  * @param {Function} fn - Function to fire
  * @param {Number} timeoutMs - time in milliseconds to buffer all calls to fn.
